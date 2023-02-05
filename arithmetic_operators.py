d26a9cc9a9498f878bc45a97b919fc06520914dd
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-05 13:23:56 -0800

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,32 @@
+# Python supports the following arithmetic operators:
+# + for addition
+# - for subtraction
+# * for multiplication
+# / for division (floating-point division)
+# // for floor division (rounds down to the nearest integer)
+# % for modulo (returns the remainder after division)
+# ** for exponentiation (raises a number to a power)
+
+# Examples of each operator:
+
+# Addition
+print(2 + 3) # 5
+
+# Subtraction
+print(5 - 2) # 3
+
+# Multiplication
+print(2 * 3) # 6
+
+# Division
+print(6 / 2) # 3.0
+
+# Floor Division
+print(6 // 2) # 3
+
+# Modulo
+print(7 % 3) # 1
+
+# Exponentiation
+print(2 ** 3) # 8
+
